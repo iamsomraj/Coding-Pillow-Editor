d85a48465f6e43058578ac57e92fddb4600c7031
Somraj Mukherjee <iamsomraj@gmail.com>
2021-06-06 00:21:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Container } from "react-bootstrap/";
-import "./App.css";
 import CodeEditor from "./components/CodeEditor/CodeEditor";
 import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
