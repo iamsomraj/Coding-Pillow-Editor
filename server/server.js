da438b95fdf69f820f7aba19c43a16ab77109f8b
Somraj Mukherjee <iamsomraj@gmail.com>
2021-06-09 08:55:21 +0530

    added some checks for prod

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
@@ -14,6 +15,21 @@ app.use(express.json());
 
 app.use("/api/files", fileRoutes);
 app.use("/api/users", userRoutes);
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 
 app.use(pageNotFound);
 app.use(errorHandler);
