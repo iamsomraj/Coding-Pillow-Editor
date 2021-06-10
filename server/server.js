42d41c237e219ee6bf4d32119d04b3f259f080ca
Somraj Mukherjee <iamsomraj@gmail.com>
2021-06-11 00:44:40 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  5000,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+});
